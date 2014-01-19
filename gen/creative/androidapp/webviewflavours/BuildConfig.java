cd6653b746e7cde1efd20d3c00ec98d58d4436ec
Juan de la Cruz García García <jcruzgar@gmail.com>
2014-01-19 02:13:34 +0100

    First commmit

---
+++ gen/creative/androidapp/webviewflavours/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package creative.androidapp.webviewflavours;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
